51bdb2e4d0fa256b7d2e7bffe9810fd5a384b061
Alexander Robertson <xanrob@me.com>
2023-10-03 16:51:33 -0400

    Disable auto-select for autocomplete

--- config.lua
+++ config.lua
@@ -29,7 +29,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<CR>"] = cmp.mapping.confirm(),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
