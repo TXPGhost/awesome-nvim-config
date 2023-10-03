300e99462411254351de423f0d9f2adebae0de85
Alexander Robertson <xanrob@me.com>
2023-10-03 13:43:15 -0400

    Added error/warn/info icons to LSP gutter

--- config.lua
+++ config.lua
@@ -1,3 +1,10 @@
+-- set diagnostic icons
+
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+
 -- nvim-cmp
 local cmp = require("cmp")
 local lspkind = require("lspkind")
