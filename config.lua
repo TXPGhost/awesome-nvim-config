5e1e9478dddff7db08fe218276423b0f3ef2c741
Alexander Robertson <xanrob@me.com>
2023-11-23 22:10:01 -0500

    Changed diagnostic icon to lightbulb

--- config.lua
+++ config.lua
@@ -12,11 +12,7 @@ vim.opt.shm:append("I")
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = { "*.wgsl" }, command = "set filetype=wgsl" })
 
 -- configure diagnostics
-vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-vim.diagnostic.config({ severity_sort = true, virtual_text = { prefix = "" } })
+vim.diagnostic.config({ severity_sort = true, virtual_text = { prefix = "" } })
 
 -- set mouse scroll
 vim.opt.mousescroll = "ver:1,hor:2"
