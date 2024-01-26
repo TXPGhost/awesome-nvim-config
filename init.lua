1dea0b04988d94eeef5b11e140d9079621eaebeb
Alexander Robertson <xanrob@me.com>
2024-01-26 11:53:23 -0500

    Fixed tab navigation in oil buffers

--- init.lua
+++ init.lua
@@ -174,7 +174,27 @@ conform.setup({
 require("lualine").setup({})
 
 -- oil
-require("oil").setup({})
+require("oil").setup({
+	keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<C-s>"] = "actions.select_vsplit",
+		-- ["<C-h>"] = "actions.select_split",
+		["<C-t>"] = "actions.select_tab",
+		["<C-p>"] = "actions.preview",
+		["<C-c>"] = "actions.close",
+		-- ["<C-l>"] = "actions.refresh",
+		["-"] = "actions.parent",
+		["_"] = "actions.open_cwd",
+		["`"] = "actions.cd",
+		["~"] = "actions.tcd",
+		["gs"] = "actions.change_sort",
+		["gx"] = "actions.open_external",
+		["g."] = "actions.toggle_hidden",
+		["g\\"] = "actions.toggle_trash",
+	},
+	use_default_keymaps = false,
+})
 vim.keymap.set("n", "-", "<cmd>Oil<cr>zz", { desc = "Open parent directory" })
 
 -- nvim-autopairs
