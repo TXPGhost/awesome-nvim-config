604e36906fe41c96a392b06c9032334f42742c84
Alexander Robertson <xanrob@me.com>
2025-03-04 10:43:10 -0500

    add loader for performance

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- make things faster
+vim.loader.enable()
+
+-- install lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
